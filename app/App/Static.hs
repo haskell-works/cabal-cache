e3b8aff1b9ccc6a7f0cc102da3e6779b924ba02f
John Ky <newhoggy@gmail.com>
2019-04-09 17:02:53 +1000

    Home directory as a constant

---
+++ app/App/Static.hs
@@ -0,0 +1,11 @@
+module App.Static where
+
+import Data.Text (Text)
+
+import qualified Data.Text        as T
+import qualified System.Directory as IO
+import qualified System.IO.Unsafe as IO
+
+homeDirectory :: Text
+homeDirectory = IO.unsafePerformIO $ T.pack <$> IO.getHomeDirectory
+{-# NOINLINE homeDirectory #-}
