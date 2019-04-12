faa994c2417504f793bd4899828ab5893a545b55
alexeyraga <alexey.raga@gmail.com>
2019-04-12 11:10:28 +1000

    Un-template when unpacking

--- src/HaskellWorks/Ci/Assist/Tar.hs
+++ src/HaskellWorks/Ci/Assist/Tar.hs
@@ -1,12 +1,13 @@
 module HaskellWorks.Ci.Assist.Tar
 where
 
-import Codec.Archive.Tar
-import Codec.Archive.Tar.Entry
-import Data.Time.Clock.POSIX   (utcTimeToPOSIXSeconds)
-import System.Directory        (Permissions (..), getModificationTime, getPermissions)
+import           Codec.Archive.Tar
+import           Codec.Archive.Tar.Entry
+import           Data.Time.Clock.POSIX   (utcTimeToPOSIXSeconds)
+import           System.Directory        (Permissions (..), getModificationTime,
+                                          getPermissions)
 
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy    as LBS
 
 packFileEntryWith :: (LBS.ByteString -> LBS.ByteString)   -- ^ Transform file content
   -> FilePath                                             -- ^ Full path to find the file on the local disk
@@ -24,3 +25,15 @@ packFileEntryWith transform filepath tarpath = do
 
 getModTime :: FilePath -> IO EpochTime
 getModTime path = (floor . utcTimeToPOSIXSeconds) <$> getModificationTime path
+
+mapFileEntriesWith :: (FilePath -> Bool)
+  -> (LBS.ByteString -> LBS.ByteString)
+  -> Entries e
+  -> Entries e
+mapFileEntriesWith pref transform entries =
+  flip mapEntriesNoFail entries $ \entry ->
+    case entryContent entry of
+      NormalFile bs size ->
+        let bs' = transform bs
+        in entry { entryContent = NormalFile bs' (LBS.length bs') }
+      _ -> entry
