cc0ea2eb70f1698e0ad56eb9b527f0d1aafafba3
John Ky <john.ky@iohk.io>
2021-03-14 16:06:57 +1100

    New plan command

--- src/App/Commands/Options/Types.hs
+++ src/App/Commands/Options/Types.hs
@@ -19,6 +19,13 @@ data SyncToArchiveOptions = SyncToArchiveOptions
   , awsLogLevel   :: Maybe AWS.LogLevel
   } deriving (Eq, Show, Generic)
 
+data PlanOptions = PlanOptions
+  { buildPath     :: FilePath
+  , storePath     :: FilePath
+  , storePathHash :: Maybe String
+  , outputFile    :: FilePath
+  } deriving (Eq, Show, Generic)
+
 data SyncFromArchiveOptions = SyncFromArchiveOptions
   { region        :: Region
   , archiveUris   :: [Location]
