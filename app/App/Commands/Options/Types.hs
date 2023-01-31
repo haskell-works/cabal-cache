50eaf09f42e282a6b2fbbf38fd14f0e9bc966618
John Ky <john.ky@iohk.io>
2023-01-30 23:24:43 -0800

    Ignore packages CLI option for sync-to-archive

--- app/App/Commands/Options/Types.hs
+++ app/App/Commands/Options/Types.hs
@@ -13,6 +13,7 @@ import Data.ByteString                  (ByteString)
 import Data.Set                         (Set)
 import GHC.Generics                     (Generic)
 import HaskellWorks.CabalCache.Location (Location)
+import HaskellWorks.CabalCache.Types    (PackageId)
 import Network.URI                      (URI)
 
 import qualified Data.List.NonEmpty             as NEL
@@ -28,16 +29,17 @@ data CpOptions = CpOptions
   } deriving (Eq, Show, Generic)
 
 data SyncToArchiveOptions = SyncToArchiveOptions
-  { region        :: AWS.Region
-  , archiveUri    :: Location
-  , path          :: FilePath
-  , buildPath     :: FilePath
-  , storePath     :: FilePath
-  , storePathHash :: Maybe String
-  , threads       :: Int
-  , awsLogLevel   :: Maybe AWS.LogLevel
-  , hostEndpoint  :: Maybe (ByteString, Int, Bool)
-  , maxRetries    :: Int
+  { region          :: AWS.Region
+  , archiveUri      :: Location
+  , path            :: FilePath
+  , buildPath       :: FilePath
+  , storePath       :: FilePath
+  , storePathHash   :: Maybe String
+  , threads         :: Int
+  , awsLogLevel     :: Maybe AWS.LogLevel
+  , hostEndpoint    :: Maybe (ByteString, Int, Bool)
+  , maxRetries      :: Int
+  , ignorePackages  :: Set PackageId
   } deriving (Eq, Show, Generic)
 
 data PlanOptions = PlanOptions
