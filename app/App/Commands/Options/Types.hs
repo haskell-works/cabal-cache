614f1b96e604fb04dc9606c6f713b84a82d5f1f1
John Ky <john.ky@iohk.io>
2023-01-30 23:50:54 -0800

    Ignore packages CLI option for sync-from-archive

--- app/App/Commands/Options/Types.hs
+++ app/App/Commands/Options/Types.hs
@@ -17,7 +17,6 @@ import HaskellWorks.CabalCache.Types    (PackageId)
 import Network.URI                      (URI)
 
 import qualified Data.List.NonEmpty             as NEL
-import qualified HaskellWorks.CabalCache.Types  as Z
 import qualified Network.AWS                    as AWS
 
 data CpOptions = CpOptions
@@ -61,7 +60,7 @@ data SyncFromArchiveOptions = SyncFromArchiveOptions
   , awsLogLevel     :: Maybe AWS.LogLevel
   , hostEndpoint    :: Maybe (ByteString, Int, Bool)
   , maxRetries      :: Int
-  , ignorePackages  :: Set Z.PackageId
+  , ignorePackages  :: Set PackageId
   } deriving (Eq, Show, Generic)
 
 data VersionOptions = VersionOptions deriving (Eq, Show, Generic)
