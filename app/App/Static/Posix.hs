52878020a16b052850b9158d57aa627e8898b5fe
John Ky <john.ky@iohk.io>
2024-11-24 13:48:38 +1100

    Fix CI and tests

--- app/App/Static/Posix.hs
+++ app/App/Static/Posix.hs
@@ -6,6 +6,12 @@ import HaskellWorks.CabalCache.Location ((</>))
 import HaskellWorks.Prelude
 
 import qualified App.Static.Base as S
+import qualified System.Environment as IO
+import qualified System.IO.Unsafe as IO
 
 cabalDirectory :: FilePath
-cabalDirectory = S.homeDirectory </> ".cabal"
+cabalDirectory =
+  IO.unsafePerformIO do
+    IO.lookupEnv "CABAL_DIR" >>= \case
+      Just dir -> pure dir
+      Nothing -> pure $ S.homeDirectory </> ".cabal"
