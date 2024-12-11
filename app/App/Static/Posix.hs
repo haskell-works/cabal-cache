aeccf23e25cb82e7b6ba930aa5af6300a72d0309
John Ky <john.ky@iohk.io>
2024-12-12 08:02:30 +1100

    Test for new and old cabal directories

--- app/App/Static/Posix.hs
+++ app/App/Static/Posix.hs
@@ -6,7 +6,9 @@ import HaskellWorks.CabalCache.Location ((</>))
 import HaskellWorks.Prelude
 
 import qualified App.Static.Base as S
+import qualified System.Directory as IO
 import qualified System.Environment as IO
+import qualified System.IO as IO
 import qualified System.IO.Unsafe as IO
 
 cabalDirectory :: FilePath
@@ -14,4 +16,15 @@ cabalDirectory =
   IO.unsafePerformIO do
     IO.lookupEnv "CABAL_DIR" >>= \case
       Just dir -> pure dir
-      Nothing -> pure $ S.homeDirectory </> ".cabal"
+      Nothing -> do
+        let oldCabalDir = S.homeDirectory </> ".cabal"
+        let newCabaldir = S.homeDirectory </> ".local/state/cabal/store"
+        oldExists <- IO.doesDirectoryExist oldCabalDir
+        newExists <- IO.doesDirectoryExist newCabaldir
+        case (oldExists, newExists) of
+          (True, True) -> do
+            IO.hPutStrLn IO.stderr "Warning: Both ~/.cabal and ~/.local/state/cabal/store exist"
+            pure newCabaldir
+          (True, _) -> pure oldCabalDir
+          (_, True) -> pure newCabaldir
+          _ -> fail "No cabal directory found"
